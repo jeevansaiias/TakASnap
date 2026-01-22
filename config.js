40c77dcbcf4ce010e607af0f31b6a0181f3f292c
MedamSandhya <msandhyareddy1122@gmail.com>
2026-01-22 13:02:55 -0800

    Improve mobile nav and serve optimized images

--- config.js
+++ config.js
@@ -13,31 +13,31 @@ const portfolioConfig = {
             name: "FEATURED",
             active: true,
             images: [
-                { src: 'images/featured/0B0E78A1-8943-4F43-8489-DF7F358C3AFF_1_102_o.jpeg', title: 'FEATURED' },
-                { src: 'images/featured/8DB0D16A-FEFF-477D-B648-61696FE1DD24_1_201_a.jpeg', title: 'FEATURED' }
+                { src: 'images/web/featured/0B0E78A1-8943-4F43-8489-DF7F358C3AFF_1_102_o.jpeg', title: 'FEATURED' },
+                { src: 'images/web/featured/8DB0D16A-FEFF-477D-B648-61696FE1DD24_1_201_a.jpeg', title: 'FEATURED' }
             ]
         },
         landscapes: {
             name: "LANDSCAPES",
             active: true,
             images: [
-                { src: 'images/landscapes/0B0E78A1-8943-4F43-8489-DF7F358C3AFF_1_102_o.jpeg', title: 'LANDSCAPE' },
-                { src: 'images/landscapes/34579E0B-2CB5-4C3A-B9FD-FF3E57FAE196_1_102_o.jpeg', title: 'LANDSCAPE' },
-                { src: 'images/landscapes/8DB0D16A-FEFF-477D-B648-61696FE1DD24_1_201_a.jpeg', title: 'LANDSCAPE' },
-                { src: 'images/landscapes/DSC02881.jpeg', title: 'LANDSCAPE' },
-                { src: 'images/landscapes/DSC01941.jpeg', title: 'BANFF VALLEY' }
+                { src: 'images/web/landscapes/0B0E78A1-8943-4F43-8489-DF7F358C3AFF_1_102_o.jpeg', title: 'LANDSCAPE' },
+                { src: 'images/web/landscapes/34579E0B-2CB5-4C3A-B9FD-FF3E57FAE196_1_102_o.jpeg', title: 'LANDSCAPE' },
+                { src: 'images/web/landscapes/8DB0D16A-FEFF-477D-B648-61696FE1DD24_1_201_a.jpeg', title: 'LANDSCAPE' },
+                { src: 'images/web/landscapes/DSC02881.jpeg', title: 'LANDSCAPE' },
+                { src: 'images/web/landscapes/DSC01941.jpeg', title: 'BANFF VALLEY' }
             ]
         },
         companions: {
             name: "COMPANIONS",
             active: true,
             images: [
-                { src: 'images/companions/DSC01562.jpg', title: 'COMPANION' },
-                { src: 'images/companions/DSC01732.jpeg', title: 'COMPANION' },
-                { src: 'images/companions/DSC01750.jpeg', title: 'COMPANION' },
-                { src: 'images/companions/DSC01751.jpeg', title: 'COMPANION' },
-                { src: 'images/companions/DSC01753.jpeg', title: 'COMPANION' },
-                { src: 'images/companions/DSC01759.jpeg', title: 'COMPANION' }
+                { src: 'images/web/companions/DSC01562.jpg', title: 'COMPANION' },
+                { src: 'images/web/companions/DSC01732.jpeg', title: 'COMPANION' },
+                { src: 'images/web/companions/DSC01750.jpeg', title: 'COMPANION' },
+                { src: 'images/web/companions/DSC01751.jpeg', title: 'COMPANION' },
+                { src: 'images/web/companions/DSC01753.jpeg', title: 'COMPANION' },
+                { src: 'images/web/companions/DSC01759.jpeg', title: 'COMPANION' }
             ]
         },
         portraits: {
